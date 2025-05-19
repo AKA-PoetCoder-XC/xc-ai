21df9ceac5d86d56cfae041cbfc817d824042f1c
xiechen <1577403106@qq.com>
2025-05-19 22:16:33 +0800

    ai基本服务工程搭建

---
+++ xc-ai-service/src/main/java/com/xc/ai/controller/AiController.java
@@ -0,0 +1,19 @@
+package com.xc.ai.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "AI控制器")
+@RestController
+@RequestMapping(("/ai"))
+public class AiController {
+
+    @GetMapping("/server-info")
+    @Operation(summary = "服务基本信息")
+    public String hello() {
+        return "Hello,this is xc-ai-service controller";
+    }
+}
